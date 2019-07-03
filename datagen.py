e7ede225b78f5c5e34ea53979503d7a12d6da8a8
ischeinkman <scheinkman.ilan@gmail.com>
2019-07-03 14:31:57 -0700

    Added conlog parsing and md5 methods.

--- datagen.py
+++ datagen.py
@@ -1,7 +1,7 @@
 import sys 
 import time
-
-
+import hashlib
+import os
 
 def parse_timestamp(raw):
     month = int(raw[0:2])
@@ -25,10 +25,13 @@ def parse_xfer_log(logpath):
                 prefix = 'File Transfer Download'
             else: 
                 continue
+            is_peer = 'peer' if '(peer stats from starter)' in line else 'client'
             entry = {}
             timestamp_raw = line[:18]
             entry['kind'] = prefix.split(' ')[-1]
             entry['timestamp'] = parse_timestamp(timestamp_raw)
+            entry['ClusterId'] = line.split(' ')[3][1:-2]
+            entry['povsource'] = is_peer
             data_start_idx = line.index(prefix) + len(prefix) + 1
             data_components = line[data_start_idx : ].replace('\n', '').strip().split(' ')
             pairs_count = int(len(data_components)/2)
@@ -79,26 +82,195 @@ def data_by_dest(rows, with_direction=True):
     return retval
 
 def speed_categorize(rows):
-    retval = {'inf' : 0, 'mb_s' : 0, 'kb_s' : 0, 'b_s' :0}
+    retval = {'inf' : 0, 'mb_s' : 0, 'kb_s' : 0, 'b_s' :0, 'infj' : [], 'j' : []}
     for ent in rows: 
         speed = ent['bytes_per_second']
+        job =ent['ClusterId'] + '.' + ent['JobId']
+        retval['j'].append(job)
         if speed == 'INF':
             retval['inf'] += 1
+            retval['infj'].append(job)
         elif speed > (1024 * 1024):
             retval['mb_s'] += 1
         elif speed > 1024:
             retval['kb_s'] += 1
         else: 
             retval['b_s'] += 1
+        if len(ent['JobId'].strip()) == 0:
+            retval['nj'] += 1
     return retval 
+
+def not_initial_send(entry):
+    if entry['kind'].lower()[0] != 'u':
+        return True 
+    elif entry['povsource'] != 'client':
+        return True 
+    else: 
+        return False 
+    return not(entry['povsource'] == 'client' and entry['kind'].lower()[0] == 'u')
+
+
+def make_file_md5(name):
+    fh = open(name, 'r')
+    data = fh.read() 
+    fh.close()
+    hasher = hashlib.md5()
+    hasher.update(data)
+    hsh = hasher.hexdigest()
+    return hsh
+
+def md5_check(outdir):
+    dirlist = os.listdir(outdir)
+    stdout_list = [outdir + '/' + itm for itm in dirlist if itm.endswith('.out')]
+    assert(len(stdout_list) == 1)
+    stdout_fh = open(stdout_list[0], 'r')
+
+    hashes = {}
+    lines = stdout_fh.readlines()
+    for ln in lines: 
+        if not ln.startswith('File ') or not ' ||| MD5 ' in ln: 
+            continue 
+        (fname, expectedhash) = ln[len('File '):].split(' ||| MD5 ', 1)
+        hashes[fname] = expectedhash
+    
+    outfiles = [fl for fl in dirlist if fl in hashes]
+    assert(len(outfiles) == len(hashes))
+
+    misses = 0
+    matches = 0
+    for fl in outfiles:
+        actual_hash = make_file_md5(outdir + '/' + fl)
+        expected_hash = hashes[fl]
+        if actual_hash != expected_hash:
+            misses += 1
+        else:
+            matches += 1
+    return (matches, misses)
+
+
+def parse_conlog(raw_data):
+    retval = []
+    cur_ent = {}
+    lines = raw_data.split('\n')
+    for ln in lines: 
+        ln = ln.strip()
+        if ln == '...':
+            if not len(cur_ent) == 0:
+                retval.append(cur_ent)
+            cur_ent = {}
+            continue
+        elif not ' = ' in ln: 
+            continue 
+        kvl = ln.split(' = ', 1)
+        if not len(kvl) == 2:
+            print('Bad ln: %s'%(ln))
+        key_raw, val_raw = kvl
+        key = key_raw.strip()
+        val = val_raw.strip().strip('"')
+        assert not key in cur_ent
+        cur_ent[key] = val
+        if key == 'EventTime':
+            cur_ent['unixtime'] = time.mktime(parse_conlog_timestamp(val))
+    return retval
+
+def parse_conlog_timestamp(ts_raw):
+    fmt = '%Y-%m-%dT%H:%M:%S'
+    return time.strptime(ts_raw, fmt)
+
+def combine_data_by_job(conlog_data, xfer_data):
+    retval = {}
+    xfer_map = data_by_jobs(xfer_data)
+    for ent in conlog_data:
+        if not 'Cluster' in ent or not 'Proc' in ent:
+            print('Bad ent!')
+            print('Keys: %s'%(str(ent.keys())))
+        entkey = ent['Cluster'] + '.' + ent['Proc']
+        if entkey in retval:
+            retval[entkey]['conlog'].append(ent)
+        else: 
+            retval[entkey] = {
+                'xfer' : xfer_map.get(entkey) or [],
+                'conlog' : [ent],
+            }
+    for ent in conlog_data:
+        entkey = ent['Cluster'] + '.' + ent['Proc']
+        dt = retval[entkey]
+        assert(len(dt['xfer']) > 0 or dt['conlog'][-1]['MyType']  == 'JobAbortedEvent')
+    return retval
+def jid_sorter(a, b):
+    aj_raw, ac_raw = a.split('.')
+    aj = int(aj_raw)
+    ac = int(ac_raw)
+    bj_raw, bc_raw = b.split('.')
+    bj = int(bj_raw)
+    bc = int(bc_raw)
+    if aj != bj:
+        return aj - bj 
+    else: 
+        return ac - bc
+
+
+def argparser(args):
+    flags = {}
+    noflag = []
+    for itm in args:
+        if not itm.startswith('--'):
+            noflag.append(itm)
+        elif '=' in itm: 
+            k,v = itm[2:].split('=', 1)
+            flags[k] = v 
+        else: 
+            flags[itm[2:]] = True
+    return (flags, noflag)
+
+def xfer_log_run(flags, noflag):
+    fname = noflag[0] if flags['xfer'] is True else flags['xfer']
+    tv = parse_xfer_log(fname)
+    add_range_info(tv)
+    add_speed_info(tv)
+    by_job = data_by_jobs(filter(not_initial_send, tv))
+    for job in by_job:
+        raw = by_job[job]
+        cont = '\n\t'.join(map(str, raw))
+        print('%s => %s\n\n'%(str(job), str(cont)))
+
+def con_log_run(flags, noflag):
+    fname = noflag[0] if flags['conlog'] is True else flags['conlog']
+    raw_data = ''
+    with open(fname, 'r') as fh:
+        raw_data = fh.read()
+    tv = parse_conlog(raw_data)
+    for ent in tv: 
+        print(ent)
+
+def comb_run(flags, noflag):
+    xfer_fname = flags['xfer']
+    xfer_data =filter(not_initial_send, parse_xfer_log(xfer_fname))
+    
+    conlog_fname = flags['conlog']
+    conlog_raw_data = ''
+    with open(conlog_fname, 'r') as fh:
+        conlog_raw_data = fh.read()
+    conlog_data = parse_conlog(conlog_raw_data)
+
+    add_range_info(xfer_data)
+    add_speed_info(xfer_data)
+
+    comb_data = combine_data_by_job(conlog_data, xfer_data)
+
+    for key in comb_data:
+        print('==============\n\n%s:\n\n'%(key))
+        conlog_cont = '\n\t\t'.join(map(str, comb_data[key]['conlog']))
+        print('\tconlog:\n\t\t%s\n\n'%(conlog_cont))
+        xfer_cont = '\n\t\t'.join(map(str, comb_data[key]['xfer']))
+        print('\txfer:\n\t\t%s\n\n'%(xfer_cont))
 
 
 if __name__ == "__main__":
-    tv = parse_xfer_log(sys.argv[1])
-    add_range_info(tv)
-    add_speed_info(tv)
-    by_dest = data_by_dest(tv, False)
-    for dest in by_dest:
-        raw = by_dest[dest]
-        print('%s => %s'%(str(dest), str(speed_categorize(raw))))
-    
+    flags, noflag = argparser(sys.argv[1:])
+    if 'xfer' in flags and 'conlog' in flags:
+        comb_run(flags, noflag)
+    elif 'xfer' in flags:
+        xfer_log_run(flags, noflag)
+    elif 'conlog' in flags:
+        con_log_run(flags, noflag)
